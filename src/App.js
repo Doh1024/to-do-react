9570d23ee2bd5fc87760e056e759bee70fb7d22e
Doh1024 <dohyeongkim001024@gmail.com>
2022-12-14 13:32:29 -0600

    to-do-list

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import './App.css';
+import Todo from "./Todo"
+import Form from "./Form"
+import FilterButton from "./FilterButton"
+
+
+
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+
+  function addTask(name) {
+  const newTask = { id: "id", name, completed: false };
+  setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+export default App;
